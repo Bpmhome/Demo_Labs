1d1481844f906798fb8b5e86ea00f48677716a9d
student <student@localhost.localdomain>
2019-02-01 17:00:29 -0600

    Update

---
+++ LabBch04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char Input = 0;
+    printf("Please enter a character: ");
+    Input = getc(stdin);
+    printf("Here is this!: ");
+    putc(Input -1, stdout);
+
+    return 0;
+}
