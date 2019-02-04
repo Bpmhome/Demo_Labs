aa2fd17c9cee49daf10e11f8396cce6c897ca447
student <student@localhost.localdomain>
2019-02-04 12:53:49 -0600

    Update

--- demo_labs/TestFolder/DemoLab2.c
+++ demo_labs/TestFolder/DemoLab2.c
@@ -8,7 +8,7 @@ int main()
 
     printf("Enter the day, month and year(<dd>-<mm>-<yyyy>):");
     scanf("%2d-%2d-%4d", &intDay, &intMonth, &intYear); //asterisk in this scenario means all.(e.x. %c%d*%c all numbers until a character.)
-    printf("Here ya go:%d/%d/%d ", intDay, intMonth, intYear);
+    printf("Here ya go:%02d/%02d/%d ", intDay, intMonth, intYear);
 
     return 0;
 }
