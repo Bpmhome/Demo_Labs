76b4f48f6d5efa6a56ae9b8e74256844905821c5
student <student@localhost.localdomain>
2019-02-14 10:34:23 -0600

    Update

--- demo_labs/ch08/Calculator.c
+++ demo_labs/ch08/Calculator.c
@@ -1,5 +1,8 @@
+//Kurtis MClain
+//2/11/2019
 #include <stdio.h>
 #include <math.h>
+//initializing the functions
 int add(int Num1, int Num2);
 int subtract(int Num1, int Num2);
 int multiply(int Num1, int Num2);
@@ -7,15 +10,18 @@ int divide(int Num1, int Num2);
 
 int main()
 {
+    //declaring my variables and assigning value.
     int intFirstNum = 0;
     int intSecondNum = 0;
     char charOperator = 'a';
     char charCont = 'a';
+    //Location for my later placed "goto"
     beginning:
+    //print and scan to display conditons and accept/locate values
     printf("Please enter your maths(Ex. 1+1,4/2): ");
     scanf("%i%c%i",&intFirstNum,&charOperator,&intSecondNum);
     switch(charOperator)
-    {
+    {//switch case using the Operator input to play the correct funtion
         case '+':
         add(intFirstNum,intSecondNum);
         break;
@@ -29,11 +35,14 @@ int main()
         divide(intFirstNum,intSecondNum);
         break;
     }
+    //printf to ask the user if they would like to continue and then place a conditon to allow this
     printf("Continue?(Y/N)");
+    //getchar mostly just works as a pause
     getchar();
+    //scanf takes the input from the user to be used appropriately
     scanf("%c",&charCont);
     if(charCont == 'Y')
-    {
+    {//if statement which if met sends the program back to the "beginning"
     goto beginning;
     }
     return 0;
@@ -44,19 +53,19 @@ int main()
 }
 
 int add(int Num1, int Num2)
-{
+{//simple functon to display the answer based off of what the user inputs
     printf("%i+%i=%i\n",Num1,Num2,Num1+Num2);
     return 0;
 }
 int subtract(int Num1, int Num2)
-{
+{//simple functon to display the answer based off of what the user inputs
     printf("%i-%i=%i\n",Num1,Num2,Num1-Num2);
 }
 int multiply(int Num1, int Num2)
-{
+{//simple functon to display the answer based off of what the user inputs
     printf("%i*%i=%i\n",Num1,Num2,Num1*Num2);
 }
 int divide(int Num1, int Num2)
-{
+{//simple functon to display the answer based off of what the user inputs
     printf("%i/%i=%i\n",Num1,Num2,Num1/Num2);
 }
--- demo_labs/ch08/PerformanceLabch08A.c
+++ demo_labs/ch08/PerformanceLabch08A.c
@@ -4,30 +4,30 @@
 //8.0
 #include <stdio.h>
 #include <string.h>
-
+//initializes the functions
 int replace_character(char * string, const char findThisChar, const char replaceItWithThis);
 int clearInput();
 
 int main()
-{
+{//declaring variables
     char inputString [256] = {0};
     char FindChar;
     char ReplaceChar;
-
+//prints conditon and scans input locating into my variable
     printf("Print a statement");
     scanf("%255s", &inputString);
-    clearInput();
-    printf("Enter the character you would like to replace and what you'd like to replace it with:");
+    clearInput();//conditional statement
+    printf("Enter the character you would like to replace and what you'd like to replace it with:");//another print and scan to state condition and take input
     scanf("%c,%c",&FindChar,&ReplaceChar);
-    replace_character(inputString, FindChar, ReplaceChar);
+    replace_character(inputString, FindChar, ReplaceChar);//replaces the located character and then prints it back out with the printf below
     printf("%s",inputString);
 
 }
 
 int replace_character(char * string, const char findThisChar, const char replaceItWithThis)
 {
-    char *current_pos = strchr(string,findThisChar);
-    while (current_pos){
+    char *current_pos = strchr(string,findThisChar);//strchr located the first instance of given character and puts it into current_pos
+    while (current_pos){//while loop to replace the currect position with the directed character
         *current_pos = replaceItWithThis;
         current_pos = strchr(current_pos,findThisChar);
     }
@@ -35,6 +35,6 @@ int replace_character(char * string, const char findThisChar, const char replace
 }
 
 int clearInput()
-{
+{//checks for validation of input before continuing
     while ( getchar() != '\n' );
 }
